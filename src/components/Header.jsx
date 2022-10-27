348185f6aa0d7edf05e258540b8bc834b48bbfe3
madhavbhatia01 <71606419+madhavbhatia01@users.noreply.github.com>
2022-10-28 02:14:12 +0530

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import EventNoteIcon from "@mui/icons-material/EventNote";
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <EventNoteIcon />
+        Notes
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
